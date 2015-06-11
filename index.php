b539c936467734701a52a5ab2a1dd473db2e3b12
Vinicius Hacebe <vinicius_hacebe@hotmail.com>
2015-06-11 02:23:37 -0300

    layout melhorado, css e html completos (menu, footer, barra lateral e texto)

--- index.php
+++ index.php
@@ -10,29 +10,57 @@
 
 	<div class="wrapper">
 		<div class="header">
-			<div>Logo</div>
+			<div><h1>Logo</h1></div>
 			<nav>
 				<li>HOME</li>
 				<li>FESTIVAL
 					<ul>
 						<li><a href="#">Festival 2015</a></li>
-						<li><a href="#">História</a></li>
-						<li></li>
-						<li></li>
+						<li><a href="#">História</a></li>						
 					</ul>
 				</li>
-				<li>ATRAÇÕES</li>
+				<li>ATRAÇÕES
+					<ul>
+						<li><a href="#">Atrações 2015</a></li>
+						<li><a href="#">Já passaram por aqui</a></li>
+					</ul>
+				</li>
 				<li>PROGRAMAÇÃO</li>
-				<li>PALCOS</li>
-				<li>GALERIAS</li>
+				<li>PALCOS
+					<ul>
+						<li><a href="#">Palco1</a></li>
+						<li><a href="#">Palco2</a></li>
+						<li><a href="#">Palco3</a></li>
+						<li><a href="#">Palco4</a></li>		
+					</ul>
+				</li>
+				<li>GALERIAS
+					<ul>
+						<li><a href="#">2013</a></li>
+						<li><a href="#">2014</a></li>
+						<li><a href="#">2015</a></li>						
+					</ul>
+				</li>
 				<li>COMO CHEGAR</li>
-				<li>ESTADIA E SERVIÇOS</li>
+				<li>ESTADIA E SERVIÇOS
+					<ul>
+						<li><a href="#">Como chegar</a></li>
+						<li><a href="#">Hotéis</a></li>
+						<li><a href="#">Restaurantes</a></li>
+					</ul>
+				</li>
 				<li>CONTATO</li>
 			</nav>
 		</div>
 		<div class="content">
-			<div class="center">&nbsp;</div>
-			<div class="right">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis eveniet, consequuntur sit nulla animi, nam voluptatem fugit magnam facere porro ipsum accusantium. Praesentium fugit doloremque neque, porro officiis nesciunt quod, eligendi, id debitis repellendus culpa dolore consequuntur nemo inventore deleniti at. Est, ipsam, quas. Aliquam assumenda totam veritatis laboriosam sunt, ad perferendis velit eos, perspiciatis dolore repudiandae culpa nesciunt possimus fuga consequatur, dolorem cum quibusdam at quasi soluta est pariatur deserunt eum! Iure laudantium quod et eum quos incidunt consequuntur reiciendis hic ullam consequatur adipisci officia, nihil laboriosam, reprehenderit id veritatis ea cumque dolores ab quaerat neque repellat quas harum. Similique sequi quaerat qui nulla id minima mollitia alias tempora maxime possimus atque molestias obcaecati inventore quas error a iusto ea delectus earum debitis, quos ad, quia voluptates optio unde! Neque quo illo nostrum tempore architecto nobis possimus id corporis, iure saepe autem rem odio sequi nemo doloribus, deserunt consequuntur! Explicabo possimus aliquid ipsum voluptates praesentium libero, id nobis, odit cum nostrum hic exercitationem reiciendis quia officiis nihil maiores. Quisquam perferendis a dolorum inventore? Eius molestiae tempore autem, necessitatibus culpa. Obcaecati asperiores, voluptatem sit placeat aut, ut dolores reiciendis, dicta voluptatum maiores est ex vel, laboriosam sunt mollitia? Animi a, obcaecati, accusantium eos, nesciunt in quibusdam totam alias ex nostrum mollitia amet expedita rerum recusandae illo voluptate earum dolore quos dicta delectus cumque! Rem porro suscipit fugit corporis dolore assumenda animi explicabo tempore veritatis tempora voluptate, doloribus, rerum quaerat ratione nisi totam error ex consequuntur pariatur quas ea, sapiente optio. Suscipit asperiores, corporis soluta dolorum, mollitia quam qui sequi laudantium assumenda molestiae, sunt excepturi ipsam aliquid aperiam enim distinctio ullam. Optio inventore vero officiis recusandae porro voluptas ex deleniti ipsam blanditiis eos repellat consequuntur error praesentium numquam, sit soluta perferendis ipsum quo sed natus iste tempora. Reprehenderit cupiditate quisquam explicabo?</div>
+			<div class="center">
+				<div class="banner">Banner / Carrossel</div>
+              Aliquam assumenda totam veritatis laboriosam sunt, ad perferendis velit eos, perspiciatis dolore repudiandae culpa nesciunt possimus fuga consequatur, dolorem cum quibusdam at quasi soluta est pariatur deserunt eum! Iure laudantium quod et eum quos incidunt consequuntur reiciendis hic ullam consequatur adipisci officia, nihil laboriosam, reprehenderit id veritatis ea cumque dolores ab quaerat neque repellat quas harum. Similique sequi quaerat qui nulla id minima mollitia alias tempora maxime possimus atque molestias obcaecati inventore quas error a iusto ea delectus earum debitis, quos ad, quia voluptates optio unde! Neque quo illo nostrum tempore architecto nobis possimus id corporis, iure saepe autem rem odio sequi nemo doloribus, deserunt consequuntur! Explicabo possimus aliquid ipsum voluptates praesentium libero, id nobis, odit cum nostrum hic exercitationem reiciendis quia officiis nihil maiores. Quisquam perferendis a dolorum inventore? Eius molestiae tempore autem, necessitatibus culpa. Obcaecati asperiores, voluptatem sit placeat aut, ut dolores reiciendis, dicta voluptatum maiores est ex vel, laboriosam sunt mollitia? Animi a, obcaecati, accusantium eos, nesciunt in quibusdam totam alias ex nostrum mollitia amet expedita rerum recusandae illo voluptate earum dolore quos dicta delectus cumque! Rem porro suscipit fugit corporis dolore assumenda animi explicabo tempore veritatis tempora voluptate, doloribus, rerum quaerat ratione nisi totam error ex consequuntur pariatur quas ea, sapiente optio. Suscipit asperiores, corporis soluta dolorum, mollitia quam qui sequi laudantium assumenda molestiae, sunt excepturi ipsam aliquid aperiam enim distinctio ullam. Optio inventore vero officiis recusandae porro voluptas ex deleniti ipsam blanditiis eos repellat consequuntur error praesentium numquam, sit soluta perferendis ipsum quo sed natus iste tempora. Reprehenderit cupiditate quisquam explicabo?
+			</div>
+			<div class="right">
+				<div class="ad">Anúncio</div>
+	  			<div class="ad">Anúncio</div>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis eveniet, consequuntur sit nulla animi, nam voluptatem fugit magnam facere porro ipsum accusantium. Praesentium fugit doloremque neque, porro officiis nesciunt quod, eligendi, id debitis repellendus culpa dolore consequuntur nemo inventore deleniti at. Est, ipsam, quas.</div>
 		</div>
 		<div class="footer"></div>
 
