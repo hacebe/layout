7e12713c1bf6a3da0d6ce4c5aca22d20b63a94ad
Vinicius Hacebe <hacebe@gmail.com>
2015-06-18 10:39:25 -0300

    Black Stars HTML template

--- index.php
+++ index.php
@@ -6,7 +6,15 @@
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	
+	<div id="fb-root"></div>
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3&appId=178362485577477";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
 
 	<div class="wrapper">
 		<div class="header">
@@ -55,12 +63,21 @@
 		<div class="content">
 			<div class="center">
 				<div class="banner">Banner / Carrossel</div>
-              Aliquam assumenda totam veritatis laboriosam sunt, ad perferendis velit eos, perspiciatis dolore repudiandae culpa nesciunt possimus fuga consequatur, dolorem cum quibusdam at quasi soluta est pariatur deserunt eum! Iure laudantium quod et eum quos incidunt consequuntur reiciendis hic ullam consequatur adipisci officia, nihil laboriosam, reprehenderit id veritatis ea cumque dolores ab quaerat neque repellat quas harum. Similique sequi quaerat qui nulla id minima mollitia alias tempora maxime possimus atque molestias obcaecati inventore quas error a iusto ea delectus earum debitis, quos ad, quia voluptates optio unde! Neque quo illo nostrum tempore architecto nobis possimus id corporis, iure saepe autem rem odio sequi nemo doloribus, deserunt consequuntur! Explicabo possimus aliquid ipsum voluptates praesentium libero, id nobis, odit cum nostrum hic exercitationem reiciendis quia officiis nihil maiores. Quisquam perferendis a dolorum inventore? Eius molestiae tempore autem, necessitatibus culpa. Obcaecati asperiores, voluptatem sit placeat aut, ut dolores reiciendis, dicta voluptatum maiores est ex vel, laboriosam sunt mollitia? Animi a, obcaecati, accusantium eos, nesciunt in quibusdam totam alias ex nostrum mollitia amet expedita rerum recusandae illo voluptate earum dolore quos dicta delectus cumque! Rem porro suscipit fugit corporis dolore assumenda animi explicabo tempore veritatis tempora voluptate, doloribus, rerum quaerat ratione nisi totam error ex consequuntur pariatur quas ea, sapiente optio. Suscipit asperiores, corporis soluta dolorum, mollitia quam qui sequi laudantium assumenda molestiae, sunt excepturi ipsam aliquid aperiam enim distinctio ullam. Optio inventore vero officiis recusandae porro voluptas ex deleniti ipsam blanditiis eos repellat consequuntur error praesentium numquam, sit soluta perferendis ipsum quo sed natus iste tempora. Reprehenderit cupiditate quisquam explicabo?
+              	<p>Aliquam assumenda totam veritatis laboriosam sunt, ad perferendis velit eos, perspiciatis dolore repudiandae culpa nesciunt possimus fuga consequatur, dolorem cum quibusdam at quasi soluta est pariatur deserunt eum! Iure laudantium quod et eum quos incidunt consequuntur reiciendis hic ullam consequatur adipisci officia, nihil laboriosam, reprehenderit id veritatis ea cumque dolores ab quaerat neque repellat quas harum. Similique sequi quaerat qui nulla id minima mollitia alias tempora maxime possimus atque molestias obcaecati inventore quas error a iusto ea delectus earum debitis, quos ad, quia voluptates optio unde! Neque quo illo nostrum tempore architecto nobis possimus id corporis, iure saepe autem rem odio sequi nemo doloribus, deserunt consequuntur! Explicabo possimus aliquid ipsum voluptates praesentium libero, id nobis, odit cum nostrum hic exercitationem reiciendis quia officiis nihil maiores. Quisquam perferendis a dolorum inventore? Eius molestiae tempore autem, necessitatibus culpa. Obcaecati asperiores, voluptatem sit placeat aut, ut dolores reiciendis, dicta voluptatum maiores est ex vel, laboriosam sunt mollitia? Animi a, obcaecati, accusantium eos, nesciunt in quibusdam totam alias ex nostrum mollitia amet expedita rerum recusandae illo voluptate earum dolore quos dicta delectus cumque! Rem porro suscipit fugit corporis dolore assumenda animi explicabo tempore veritatis tempora voluptate, doloribus, rerum quaerat ratione nisi totam error ex consequuntur pariatur quas ea, sapiente optio. Suscipit asperiores, corporis soluta dolorum, mollitia quam qui sequi laudantium assumenda molestiae, sunt excepturi ipsam aliquid aperiam enim distinctio ullam. Optio inventore vero officiis recusandae porro voluptas ex deleniti ipsam blanditiis eos repellat consequuntur error praesentium numquam, sit soluta perferendis ipsum quo sed natus iste tempora. Reprehenderit cupiditate quisquam explicabo?</p>	
+              	<p> 
+              		<h1>Título Exemplo</h1>             		
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. At nam, culpa voluptas nulla tempore corporis sunt accusantium aperiam labore enim nihil neque, ducimus, doloremque beatae! Voluptates beatae sit voluptatibus dicta, minus, exercitationem non dignissimos sed veniam vitae doloremque mollitia voluptatem molestias vel porro. Laudantium similique fugiat nesciunt libero, natus in quidem! Sit commodi, aliquam fuga.
+            	</p>
 			</div>
 			<div class="right">
 				<div class="ad">Anúncio</div>
 	  			<div class="ad">Anúncio</div>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis eveniet, consequuntur sit nulla animi, nam voluptatem fugit magnam facere porro ipsum accusantium. Praesentium fugit doloremque neque, porro officiis nesciunt quod, eligendi, id debitis repellendus culpa dolore consequuntur nemo inventore deleniti at. Est, ipsam, quas.</div>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis eveniet, consequuntur sit nulla animi, nam voluptatem fugit magnam facere porro ipsum accusantium. Praesentium fugit doloremque neque, porro officiis nesciunt quod, eligendi, id debitis repellendus culpa dolore consequuntur nemo inventore deleniti at. Est, ipsam, quas.</p>
+				<p>
+				<div class="fb-page" data-href="https://www.facebook.com/retvbrazil" data-width="550" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/retvbrazil"><a href="https://www.facebook.com/retvbrazil">Retv Brazil</a></blockquote></div></div>
+				</p>
+				<!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, ab. Voluptates veniam voluptatibus totam sunt officiis. Omnis aut id laborum ducimus vero tempore, et voluptatum quos consectetur iusto, velit excepturi quidem obcaecati, natus? Qui voluptatum laudantium cum voluptate nam quis explicabo delectus, tempore, odio eius voluptas tenetur deleniti error natus mollitia dolore laborum repudiandae incidunt debitis blanditiis similique, fugit veniam omnis nesciunt! Dicta, cum obcaecati. Provident architecto soluta, porro accusamus odio molestiae voluptatum voluptatem labore quas at necessitatibus veniam, numquam nisi eveniet a consectetur illo impedit dicta quod. Impedit enim error asperiores et molestiae dolorum mollitia libero, nobis doloremque, ipsam fugiat sapiente provident quia eligendi iste incidunt, obcaecati. Nostrum, aliquid recusandae minima asperiores at nisi maiores perspiciatis! Libero vel, soluta magni, impedit eveniet a eos assumenda rerum sed unde dolorum? Quia provident, nulla autem a. Repudiandae eos dicta officia ab incidunt nostrum, quibusdam ipsum unde natus cupiditate, ratione, perferendis repellendus!</p> -->
+			</div>
 		</div>
 		<div class="footer"></div>
 
